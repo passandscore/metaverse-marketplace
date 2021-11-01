cc7acb384ff4ae054a3a6cdb0170a498b093dcad
Jason Schwarz <jason.c.schwarz@gmail.com>
2021-11-01 14:18:42 -0300

    Coding the My Assets Page

---
+++ pages/my-assets.js
@@ -0,0 +1,81 @@
+// https://reactjs.org/docs/getting-started.html
+// https://www.npmjs.com/package/web3modal
+// https://docs.ethers.io/v5/
+// https://www.npmjs.com/package/ipfs-http-client
+// https://infura.io/docs
+
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
+
+import { nftmarketaddress, nftaddress } from "../config";
+
+import Market from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+
+export default function MyAssets() {
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+
+  async function loadNFTs() {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+
+    // msg.sender is the address of the user who is currently logged in
+    // It is used to get the user's NFTs by the smart contract
+    // This is why a signer is required
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const marketContract = new ethers.Contract(
+      nftmarketaddress,
+      Market.abi,
+      signer
+    );
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
+    const data = await marketContract.fetchMyNFTs();
+
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await tokenContract.tokenURI(i.tokenId);
+        const meta = await axios.get(tokenUri);
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+        };
+        return item;
+      })
+    );
+    setNfts(items);
+    setLoadingState("loaded");
+  }
+  if (loadingState === "loaded" && !nfts.length)
+    return <h1 className="py-10 px-20 text-3xl">No assets owned</h1>;
+  return (
+    <div className="flex justify-center">
+      <div className="p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-xl overflow-hidden">
+              <img src={nft.image} className="rounded" />
+              <div className="p-4 bg-black">
+                <p className="text-2xl font-bold text-white">
+                  Price - {nft.price} Eth
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
